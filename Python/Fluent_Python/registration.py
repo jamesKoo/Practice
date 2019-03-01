ef0e589fce1b9458595ecc5698605ed8da305c59
jmkoo <jmkoo@satreci.com>
2019-03-02 00:54:49 +0900

    Update ~ 2019.02

---
+++ Python/Fluent_Python/registration.py
@@ -0,0 +1,18 @@
+registry = []
+
+def register(func):
+    print('running register(%s)' % func)
+    registry.append(func)
+    return func
+
+@register
+def f1():
+    print('running f1()')
+    
+@register
+def f2():
+    print('running f2()')
+    
+def f3():
+    print('running f3()')
+    
